528f6b26ab26502ad7b9177e01b4a0b643361bcc
sravanthi-daiv <smadd25@unh.newhaven.edu>
2024-11-11 12:06:12 -0500

    finance page

--- app/src/main/java/com/example/budgetbuddy/Finalinc.java
+++ app/src/main/java/com/example/budgetbuddy/Finalinc.java
@@ -1,10 +1,8 @@
-package com.example.myapplication;
+package com.example.budgetbuddy;
 
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.TextView;
 
